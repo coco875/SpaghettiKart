1363aad385cc97d2191ba25c5d87b6eda952e04c
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-05-16 00:24:15 -0300

    progress

--- src/port/ui/PortMenu.cpp
+++ src/port/ui/PortMenu.cpp
@@ -15,10 +15,10 @@
 #endif
 
 extern "C" {
-extern s32 gGamestateNext;
-extern s32 gMenuSelection;
-#include "audio/external.h"
-#include "defines.h"
+    extern s32 gGamestateNext;
+    extern s32 gMenuSelection;
+    #include "audio/external.h"
+    #include "defines.h"
 }
 
 namespace GameUI {
@@ -158,8 +158,9 @@ void PortMenu::AddSettings() {
                      .IsPercentage());
     AddWidget(path, "Main Music Volume: %.0f%%", WIDGET_CVAR_SLIDER_FLOAT)
         .CVar("gMainMusicVolume")
-        .Callback(
-            [](WidgetInfo& info) { audio_set_player_volume(SEQ_PLAYER_LEVEL, CVarGetFloat("gMainMusicVolume", 1.0f)); })
+        .Callback([](WidgetInfo& info) {
+            audio_set_player_volume(SEQ_PLAYER_LEVEL, CVarGetFloat("gMainMusicVolume", 1.0f));
+        })
         .Options(FloatSliderOptions()
                      .Tooltip("Adjust the background music volume.")
                      .ShowButtons(false)
@@ -167,17 +168,16 @@ void PortMenu::AddSettings() {
                      .IsPercentage());
     AddWidget(path, "Sound Effects Volume: %.0f%%", WIDGET_CVAR_SLIDER_FLOAT)
         .CVar("gSFXMusicVolume")
-        .Callback(
-            [](WidgetInfo& info) { audio_set_player_volume(SEQ_PLAYER_SFX, CVarGetFloat("gSFXMusicVolume", 1.0f)); })
-        .Options(FloatSliderOptions()
-                     .Tooltip("Adjust the sound effects volume.")
-                     .ShowButtons(false)
-                     .Format("")
-                     .IsPercentage());
+        .Callback([](WidgetInfo& info) {
+            audio_set_player_volume(SEQ_PLAYER_SFX, CVarGetFloat("gSFXMusicVolume", 1.0f));
+        })
+        .Options(
+            FloatSliderOptions().Tooltip("Adjust the sound effects volume.").ShowButtons(false).Format("").IsPercentage());
     AddWidget(path, "Sound Effects Volume: %.0f%%", WIDGET_CVAR_SLIDER_FLOAT)
         .CVar("gEnvironmentVolume")
-        .Callback(
-            [](WidgetInfo& info) { audio_set_player_volume(SEQ_PLAYER_ENV, CVarGetFloat("gEnvironmentVolume", 1.0f)); })
+        .Callback([](WidgetInfo& info) {
+            audio_set_player_volume(SEQ_PLAYER_ENV, CVarGetFloat("gEnvironmentVolume", 1.0f));
+        })
         .Options(FloatSliderOptions()
                      .Tooltip("Adjust the environment volume.")
                      .ShowButtons(false)
@@ -258,11 +258,11 @@ void PortMenu::AddSettings() {
             if (mPortMenu->disabledMap.at(DISABLE_FOR_MATCH_REFRESH_RATE_ON).active)
                 info.activeDisables.push_back(DISABLE_FOR_MATCH_REFRESH_RATE_ON);
         })
-        .Options(IntSliderOptions().Tooltip(tooltip).Min(30).Max(maxFps).DefaultValue(30));
+        .Options(IntSliderOptions().Tooltip(tooltip).Min(20).Max(maxFps).DefaultValue(20));
     AddWidget(path, "Match Refresh Rate", WIDGET_BUTTON)
         .Callback([](WidgetInfo& info) {
             int hz = Ship::Context::GetInstance()->GetWindow()->GetCurrentRefreshRate();
-            if (hz >= 30 && hz <= 360) {
+            if (hz >= 20 && hz <= 360) {
                 CVarSetInteger("gInterpolationFPS", hz);
                 Ship::Context::GetInstance()->GetWindow()->GetGui()->SaveConsoleVariablesNextFrame();
             }
@@ -332,67 +332,54 @@ void PortMenu::AddEnhancements() {
     AddMenuEntry("Enhancements", "gSettings.Menu.EnhancementsSidebarSection");
     WidgetPath path = { "Enhancements", "General", SECTION_COLUMN_1 };
     AddSidebarEntry("Enhancements", "General", 3);
-    // UIWidgets::WindowButton("Multiplayer", "gMultiplayerWindowEnabled", GameUI::mMultiplayerWindow,
-    //                         { .tooltip = "Shows the multiplayer window" });
-    //     UIWidgets::WindowButton("Freecam", "gFreecam", GameUI::mFreecamWindow,
-    //                             { .tooltip = "Allows you to fly around the course" });
+        //UIWidgets::WindowButton("Multiplayer", "gMultiplayerWindowEnabled", GameUI::mMultiplayerWindow,
+        //                        { .tooltip = "Shows the multiplayer window" });
+        //    UIWidgets::WindowButton("Freecam", "gFreecam", GameUI::mFreecamWindow,
+        //                            { .tooltip = "Allows you to fly around the course" });
     AddWidget(path, "No multiplayer feature cuts", WIDGET_CVAR_CHECKBOX)
         .CVar("gMultiplayerNoFeatureCuts")
         .Options(CheckboxOptions().Tooltip("Allows full train and jumbotron in multiplayer, etc."));
-    AddWidget(path, "Widescreen portrait spacing", WIDGET_CVAR_CHECKBOX)
-        .CVar("gBetterResultPortraits")
-        .Options(CheckboxOptions().Tooltip("Alters result portrait spacing for better aesthetics on widescreen"));
+    AddWidget(path, "General Improvements", WIDGET_CVAR_CHECKBOX)
+        .CVar("gImprovements").Options(CheckboxOptions().Tooltip("General improvements to the game experience."));
     AddWidget(path, "No Level of Detail (LOD)", WIDGET_CVAR_CHECKBOX)
         .CVar("gDisableLod")
-        .Options(CheckboxOptions().Tooltip(
-            "Disable Level of Detail (LOD) to avoid models using lower poly versions at a distance"));
+        .Options(CheckboxOptions().Tooltip("Disable Level of Detail (LOD) to avoid models using lower poly versions at a distance"));
     AddWidget(path, "Disable Culling", WIDGET_CVAR_CHECKBOX)
-        .CVar("gNoCulling")
-        .Options(CheckboxOptions().Tooltip("Disable original culling of mk64"));
+        .CVar("gNoCulling").Options(CheckboxOptions().Tooltip("Disable original culling of mk64"));
     AddWidget(path, "Far Frustrum", WIDGET_CVAR_SLIDER_FLOAT)
         .CVar("gFarFrustrum")
-        .PreFunc([](WidgetInfo& info) { info.isHidden = !CVarGetInteger("gNoCulling", 0); })
-        .Options(FloatSliderOptions()
-                     .Min(0.0f)
-                     .Max(10000.0f)
-                     .DefaultValue(10000.0f)
-                     .Tooltip("Say how Far the Frustrum are when 'Disable Culling' are enable")
-                     .Step(10.0f));
-    AddWidget(path, "Enable Custom CC", WIDGET_CVAR_CHECKBOX).CVar("gEnableCustomCC");
-    AddWidget(path, "Custom CC", WIDGET_CVAR_SLIDER_FLOAT)
-        .CVar("gCustomCC")
-        .PreFunc([](WidgetInfo& info) { info.isHidden = !CVarGetInteger("gEnableCustomCC", 0); })
-        .Options(FloatSliderOptions().Min(0.0f).Max(1000.0f).DefaultValue(150.0f).Step(10.0f));
-
-    AddWidget(path, "Harder CPU", WIDGET_CVAR_CHECKBOX).CVar("gHarderCPU");
+        .Options(FloatSliderOptions().Min(0.0f).Max(10000.0f).DefaultValue(10000.0f)
+                    .Tooltip("Say how Far the Frustrum are when 'Disable Culling' are enable").Step(10.0f));
 
     path = { "Enhancements", "Cheats", SECTION_COLUMN_1 };
     AddSidebarEntry("Enhancements", "Cheats", 3);
-    AddWidget(path, "Moon Jump", WIDGET_CVAR_CHECKBOX).CVar("gEnableMoonJump");
+    AddWidget(path, "Moon Jump", WIDGET_CVAR_CHECKBOX)
+        .CVar("gEnableMoonJump");
+    AddWidget(path, "Enable Custom CC", WIDGET_CVAR_CHECKBOX)
+        .CVar("gEnableCustomCC");
+    AddWidget(path, "Custom CC", WIDGET_CVAR_SLIDER_FLOAT)
+        .CVar("gCustomCC")
+        .Options(FloatSliderOptions().Min(0.0f).Max(1000.0f).DefaultValue(150.0f).Step(10.0f));
     AddWidget(path, "Disable Wall Collision", WIDGET_CVAR_CHECKBOX)
         .CVar("gNoWallColision")
         .Options(CheckboxOptions().Tooltip("Disable wall collision."));
     AddWidget(path, "Min Height", WIDGET_CVAR_SLIDER_FLOAT)
         .CVar("gMinHeight")
-        .PreFunc([](WidgetInfo& info) { info.isHidden = !CVarGetInteger("gNoWallColision", 0); })
-        .Options(FloatSliderOptions().Min(-50.0f).Max(50.0f).DefaultValue(0.0f).Tooltip(
-            "When Disable Wall Collision are enable what is the minimal height you can get."));
+        .Options(FloatSliderOptions().Min(-50.0f).Max(50.0f).DefaultValue(0.0f)
+                     .Tooltip("When Disable Wall Collision are enable what is the minimal height you can get."));
 
-#if not defined(__SWITCH__) and not defined(__WIIU__)
     path = { "Enhancements", "HM64 Lab", SECTION_COLUMN_1 };
     AddSidebarEntry("Enhancements", "HM64 Lab", 4);
-    AddWidget(path, "Work in progress.", WIDGET_TEXT);
     AddWidget(path, "Enable HM64 Labs", WIDGET_CVAR_CHECKBOX)
-        .CVar("gEditorEnabled")
-        .Callback([](WidgetInfo& info) {
-            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Tools")->ToggleVisibility();
-            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Scene Explorer")->ToggleVisibility();
-            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Content Browser")->ToggleVisibility();
-            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Track Properties")->ToggleVisibility();
-            Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Properties")->ToggleVisibility();
-        })
-        .Options(UIWidgets::CheckboxOptions({ { .tooltip = "Edit the universe!" } }));
-#endif
+    .CVar("gEditorEnabled")
+    .Callback([](WidgetInfo& info) {
+        Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Tools")->ToggleVisibility();
+        Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Scene Explorer")->ToggleVisibility();
+        Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Content Browser")->ToggleVisibility();
+        Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Track Properties")->ToggleVisibility();
+        Ship::Context::GetInstance()->GetWindow()->GetGui()->GetGuiWindow("Properties")->ToggleVisibility();
+    })
+    .Options(UIWidgets::CheckboxOptions({{ .tooltip = "Edit the universe!"}}));
 }
 
 #ifdef __SWITCH__
@@ -413,11 +400,11 @@ void PortMenu::AddDevTools() {
     WidgetPath path = { "Developer", "General", SECTION_COLUMN_1 };
 #ifdef __SWITCH__
     AddWidget(path, "Switch CPU Profile", WIDGET_CVAR_COMBOBOX)
-        .CVar("gSwitchPerfMode")
-        .Options(ComboboxOptions()
-                     .Tooltip("Switches the CPU profile to a different one")
-                     .ComboMap(switchCPUProfiles)
-                     .DefaultIndex(Ship::SwitchProfiles::STOCK))
+    .CVar("gSwitchPerfMode")
+    .Options(ComboboxOptions()
+        .Tooltip("Switches the CPU profile to a different one")
+        .ComboMap(switchCPUProfiles)
+        .DefaultIndex(Ship::SwitchProfiles::STOCK))
         .Callback([](WidgetInfo& info) { Ship::Switch::ApplyOverclock(); });
 #endif
     AddWidget(path, "Popout Menu", WIDGET_CVAR_CHECKBOX)
@@ -426,17 +413,6 @@ void PortMenu::AddDevTools() {
     AddWidget(path, "Debug Mode", WIDGET_CVAR_CHECKBOX)
         .CVar("gEnableDebugMode")
         .Options(CheckboxOptions().Tooltip("Enables Debug Mode."));
-    AddWidget(path, "Modify Interpolation Target FPS", WIDGET_CVAR_CHECKBOX)
-        .CVar("gModifyInterpolationTargetFPS")
-        .Options(CheckboxOptions().Tooltip("For testing frame interpolation."));
-    AddWidget(path, "Interpolation Target FPS", WIDGET_CVAR_SLIDER_INT)
-        .CVar("gInterpolationTargetFPS")
-        .PreFunc([](WidgetInfo& info) { info.isHidden = !CVarGetInteger("gModifyInterpolationTargetFPS", 0); })
-        .Options(IntSliderOptions()
-                     .Tooltip("Sets the target FPS for interpolation. When Modify Interpolation Target FPS are enable")
-                     .Min(15)
-                     .Max(360)
-                     .DefaultValue(60));
     AddWidget(path, "Render Collision", WIDGET_CVAR_CHECKBOX)
         .CVar("gRenderCollisionMesh")
         .Options(CheckboxOptions().Tooltip("Renders the collision mesh instead of the course mesh"));
@@ -453,8 +429,7 @@ void PortMenu::AddDevTools() {
     AddSidebarEntry("Developer", "Stats", 1);
     AddWidget(path, "Popout Stats", WIDGET_WINDOW_BUTTON)
         .CVar("gStatsEnabled")
-        .Options(ButtonOptions().Tooltip(
-            "Shows the stats window, with your FPS and frametimes, and the OS you're playing on"))
+        .Options(ButtonOptions().Tooltip("Shows the stats window, with your FPS and frametimes, and the OS you're playing on"))
         .WindowName("Stats");
 
     path = { "Developer", "Console", SECTION_COLUMN_1 };
@@ -470,9 +445,9 @@ PortMenu::PortMenu(const std::string& consoleVariable, const std::string& name)
     : Menu(consoleVariable, name, 0, UIWidgets::Colors::LightBlue) {
 }
 
-// bool CheckNetworkConnected(disabledInfo& info) {
-//     return gNetwork.isConnected;
-// }
+//bool CheckNetworkConnected(disabledInfo& info) {
+//    return gNetwork.isConnected;
+//}
 
 void PortMenu::InitElement() {
     Ship::Menu::InitElement();
@@ -556,4 +531,4 @@ void PortMenu::Draw() {
 void PortMenu::DrawElement() {
     Ship::Menu::DrawElement();
 }
-} // namespace GameUI
+} // namespace BenGui
