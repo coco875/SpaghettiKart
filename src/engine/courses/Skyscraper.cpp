cc24d181501d005eb4218856aa75459bf7561e24
coco875 <pereira.jannin@gmail.com>
2025-11-26 23:28:16 +0100

    fix skyscraper

--- src/engine/courses/Skyscraper.cpp
+++ src/engine/courses/Skyscraper.cpp
@@ -34,29 +34,29 @@ extern "C" {
 }
 
 const course_texture skyscraper_textures[] = {
-    { gTexture6457D8, 0x0160, 0x0800, 0x0 },
-    { gTexture6462C0, 0x01B9, 0x0800, 0x0 },
-    { gTexture6864E8, 0x0807, 0x1000, 0x0 },
-    { gTexture686CF0, 0x08B7, 0x1000, 0x0 },
-    { gTexture6875A8, 0x093F, 0x1000, 0x0 },
-    { gTexture687EE8, 0x0883, 0x1000, 0x0 },
-    { gTexture68876C, 0x0AC2, 0x1000, 0x0 },
-    { gTexture689230, 0x09CE, 0x1000, 0x0 },
-    { gTexture689C00, 0x0884, 0x1000, 0x0 },
-    { gTexture68A484, 0x07D6, 0x1000, 0x0 },
-    { gTexture68AC5C, 0x0A47, 0x1000, 0x0 },
-    { gTexture68B6A4, 0x07C5, 0x1000, 0x0 },
-    { gTexture68BE6C, 0x04A1, 0x1000, 0x0 },
-    { gTexture68C310, 0x030D, 0x1000, 0x0 },
-    { gTexture64B8D8, 0x0177, 0x0800, 0x0 },
-    { gTexture645660, 0x0176, 0x0800, 0x0 },
-    { gTextureNumberYellowBlue1, 0x01AE, 0x0800, 0x0 },
-    { gTextureNumberYellowBlue2, 0x01C6, 0x0800, 0x0 },
-    { gTextureNumberYellowBlue3, 0x01CC, 0x0800, 0x0 },
-    { gTextureNumberYellowBlue4, 0x01E4, 0x0800, 0x0 },
-    { gTexture668608, 0x0120, 0x0800, 0x0 },
-    { gTexture67B75C, 0x0108, 0x0800, 0x0 },
-    { gTexture6835F0, 0x0252, 0x1000, 0x0 },
+    { gTexture6457D8, 0x0160, 0x0800, 0x0 }, // 0x05000000
+    { gTexture6462C0, 0x01B9, 0x0800, 0x0 }, // 0x05000800
+    { gTexture6864E8, 0x0807, 0x1000, 0x0 }, // 0x05001000
+    { gTexture686CF0, 0x08B7, 0x1000, 0x0 }, // 0x05002000
+    { gTexture6875A8, 0x093F, 0x1000, 0x0 }, // 0x05003000
+    { gTexture687EE8, 0x0883, 0x1000, 0x0 }, // 0x05004000
+    { gTexture68876C, 0x0AC2, 0x1000, 0x0 }, // 0x05005000
+    { gTexture689230, 0x09CE, 0x1000, 0x0 }, // 0x05006000
+    { gTexture689C00, 0x0884, 0x1000, 0x0 }, // 0x05007000
+    { gTexture68A484, 0x07D6, 0x1000, 0x0 }, // 0x05008000
+    { gTexture68AC5C, 0x0A47, 0x1000, 0x0 }, // 0x05009000
+    { gTexture68B6A4, 0x07C5, 0x1000, 0x0 }, // 0x0500A000
+    { gTexture68BE6C, 0x04A1, 0x1000, 0x0 }, // 0x0500B000
+    { gTexture68C310, 0x030D, 0x1000, 0x0 }, // 0x0500C000
+    { gTexture64B8D8, 0x0177, 0x0800, 0x0 }, // 0x0500D000
+    { gTexture645660, 0x0176, 0x0800, 0x0 }, // 0x0500D800
+    { gTextureNumberYellowBlue1, 0x01AE, 0x0800, 0x0 }, // 0x0500E000
+    { gTextureNumberYellowBlue2, 0x01C6, 0x0800, 0x0 }, // 0x0500E800
+    { gTextureNumberYellowBlue3, 0x01CC, 0x0800, 0x0 }, // 0x0500F000
+    { gTextureNumberYellowBlue4, 0x01E4, 0x0800, 0x0 }, // 0x0500F800
+    { gTexture668608, 0x0120, 0x0800, 0x0 }, // 0x05010000
+    { gTexture67B75C, 0x0108, 0x0800, 0x0 }, // 0x05010800
+    { gTexture6835F0, 0x0252, 0x1000, 0x0 }, // 0x05011000
     { 0x00000000, 0x0000, 0x0000, 0x0 },
 };
 
@@ -141,9 +141,9 @@ void Skyscraper::Load() {
     Course::Load();
 
     // d_course_skyscraper_packed_dl_1110
-    generate_collision_mesh_with_default_section_id((Gfx*) segmented_gfx_to_virtual((void*)0x07001110), 1);
+    generate_collision_mesh_with_default_section_id((Gfx*) d_course_skyscraper_packed_dl_1110, 1);
     // d_course_skyscraper_packed_dl_258
-    generate_collision_mesh_with_default_section_id((Gfx*) segmented_gfx_to_virtual((void*)0x07000258), 1);
+    generate_collision_mesh_with_default_section_id((Gfx*) d_course_skyscraper_packed_dl_258, 1);
     func_80295C6C();
 }
 
@@ -178,21 +178,21 @@ void Skyscraper::Render(struct UnkStruct_800DC5EC* arg0) {
     gSPSetGeometryMode(gDisplayListHead++, G_SHADING_SMOOTH);
     gSPClearGeometryMode(gDisplayListHead++, G_LIGHTING);
     // d_course_skyscraper_packed_dl_FE8
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x07000FE8)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_FE8);
     // d_course_skyscraper_packed_dl_C60
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x07000C60)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_C60);
     // d_course_skyscraper_packed_dl_B70
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x07000B70)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_B70);
     // d_course_skyscraper_packed_dl_6B8
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x070006B8)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_6B8);
     // d_course_skyscraper_packed_dl_570
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x07000570)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_570);
     gSPClearGeometryMode(gDisplayListHead++, G_CULL_BACK);
     // d_course_skyscraper_packed_dl_10C8
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x070010C8)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_10C8);
     gSPSetGeometryMode(gDisplayListHead++, G_CULL_BACK);
     // d_course_skyscraper_packed_dl_258
-    gSPDisplayList(gDisplayListHead++, (segmented_gfx_to_virtual((void*)0x07000258)));
+    gSPDisplayList(gDisplayListHead++, (Gfx*) d_course_skyscraper_packed_dl_258);
 }
 
 void Skyscraper::RenderCredits() {}
