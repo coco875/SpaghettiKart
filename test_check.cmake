4abbb1b37ca94c91aba80b7a21f67bc2fd4ef676
coco875 <59367621+coco875@users.noreply.github.com>
2026-07-22 16:02:07 +0200

    Delete test_check.cmake

--- test_check.cmake
+++
@@ -1,8 +0,0 @@
-include(CheckCXXCompilerFlag)
-include(CheckLinkerFlag)
-
-check_cxx_compiler_flag("-pthread" HAS_PTHREAD)
-if(HAS_PTHREAD)
-  add_compile_options(-pthread)
-  add_link_options(-pthread)
-endif()
